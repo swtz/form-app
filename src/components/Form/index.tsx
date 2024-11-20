7c5c563e9c7df027d8a5841e0a1dcbae0671cb5f
Leonardo Lüders <luderser@hotmail.com>
2024-11-20 13:02:54 -0300

    validacao de entrada usando react-hook-form e yup (maneira performatica)

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,80 +1,73 @@
-import React, { useState } from 'react';
-import { Alert } from 'react-native';
+import React from 'react';
 import { Button } from '../Button';
-import { Input } from '../Input';
+import { ControlledInput } from '../ControlledInput';
 import { Container } from './styles';
+import { useForm } from 'react-hook-form';
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
-const VALID_EMAIL_EXPRESSION = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([\t]*\r\n)?[\t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([\t]*\r\n)?[\t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+type FormData = {
+  name: string;
+  email: string;
+  password: string;
+  password_confirm?: string;
+}
+
+const schema = yup.object({
+  name: yup.string().required("Informe seu nome"),
+  email: yup.string().email("E-mail inválido").required("Informe o e-mail"),
+  password: yup.string().min(6, "O tamanho da senha é inválido").required("Informe a senha"),
+  password_confirm: yup.string().oneOf([yup.ref('password'), undefined], "A senha de confirmação não confere")
+});
 
 export function Form() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [passwordConfirm, setPasswordConfirm] = useState("");
+  const { control, handleSubmit, formState: { errors } } = useForm<FormData>({
+    resolver: yupResolver(schema)
+  });
 
-  function handleUserRegister() {
 
-    if (name.trim() === "") {
-      return Alert.alert("Informe o nome.");
-    }
-
-    if (email.trim() === "") {
-      return Alert.alert("Informe o e-mail.");
-    }
-
-    if (!(VALID_EMAIL_EXPRESSION).test(email.toLowerCase())) {
-      return Alert.alert("E-mail inválido.");
-    }
-
-    if (password.trim() === "") {
-      return Alert.alert("Informe a senha.");
-    }
-
-    if (password.trim().length < 6) {
-      return Alert.alert("O tamanho da senha é inválido.");
-    }
-
-    if (password.trim() !== passwordConfirm.trim()) {
-      return Alert.alert("A senha de confirmação não confere.");
-    }
-
-    return Alert.alert("Cadastro realizado!");
+  function handleUserRegister(data: FormData) {
+    console.log(data);
   }
 
   return (
     <Container>
-      <Input
+      <ControlledInput
+        name="name"
+        control={control}
         icon="user"
         placeholder="Nome"
-        onChangeText={setName}
-        value={name}
+        error={errors.name}
       />
-      <Input
+      <ControlledInput
+        name="email"
+        control={control}
         icon="mail"
         placeholder="E-mail"
         keyboardType="email-address"
         autoCapitalize='none'
-        onChangeText={setEmail}
-        value={email}
+        error={errors.email}
       />
-      <Input
+      <ControlledInput
+        name="password"
+        control={control}
         icon="lock"
         placeholder="Senha"
         secureTextEntry
-        onChangeText={setPassword}
-        value={password}
+        error={errors.password}
       />
-      <Input
+      <ControlledInput
+        name="password_confirm"
+        control={control}
         icon="lock"
         placeholder="Confirme a senha"
         secureTextEntry
-        onChangeText={setPasswordConfirm}
-        value={passwordConfirm}
+        error={errors.password_confirm}
       />
 
       <Button
         title="Cadastrar"
-        onPress={handleUserRegister}
+        onPress={handleSubmit(handleUserRegister)}
       />
     </Container>
   )
